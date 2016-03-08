f5fd3e61b4a79f25713800a24d28c48a5ecfdaf8
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-03-08 13:34:41 -0800

    Add dominates relationship for the pve.

--- src/pve.erl
+++ src/pve.erl
@@ -12,6 +12,8 @@
 
 -behaviour(vector).
 
+-dialyzer([{nowarn_function, [strictly_dominates/2, dominates/2]}]).
+
 %% Record specifications.
 -record(state, {counter, exceptions = []}).
 
@@ -47,9 +49,31 @@ generate(Actor, Vector0) ->
 
 %% @doc Determine if a vector or an version is dominated by another
 %%      vector.
--spec strictly_dominates(vector(), vector()) -> boolean().
-strictly_dominates(_Vector1, _Vector2) ->
-    {error, not_implemented}.
+-spec strictly_dominates(version() | vector(), vector()) -> boolean().
+strictly_dominates({Actor, Count}, Vector) ->
+    case orddict:find(Actor, Vector) of
+        {ok, #state{counter=Counter}} ->
+            Counter > Count;
+        _ ->
+            false
+    end;
+strictly_dominates([], Vector2) when Vector2 =/= [] ->
+    true;
+strictly_dominates(Vector1, Vector2) ->
+    FoldFun = fun(Actor1, #state{counter=Count1}, {Dominates, AtLeastOneStrict}) ->
+                      case orddict:find(Actor1, Vector2) of
+                          {ok, #state{counter=Count2}} ->
+                              {Count2 >= Count1 andalso Dominates,
+                               Count2 > Count1 orelse AtLeastOneStrict};
+                          error ->
+                              {false andalso Dominates,
+                               false orelse AtLeastOneStrict}
+                      end
+              end,
+    {Dominates, AtLeastOneStrict} = orddict:fold(FoldFun, {true, false}, Vector1),
+    MoreElements = orddict:size(Vector2) > orddict:size(Vector1),
+    Dominates andalso (AtLeastOneStrict orelse MoreElements).
+
 
 %% @doc Determine if a vector or an version is dominated by another
 %%      vector.
@@ -61,8 +85,36 @@ dominates({Actor, Count}, Vector) ->
         _ ->
             false
     end;
-dominates(_Vector1, _Vector2) ->
-    {error, not_implemented}.
+dominates(Vector1, Vector2) ->
+    FoldFun = fun(Actor1, #state{counter=Count1, exceptions=Exceptions1}, AccIn) ->
+                      case orddict:find(Actor1, Vector2) of
+                          {ok, #state{counter=Count2, exceptions=Exceptions2}} ->
+                              dominates(Actor1,
+                                        Vector1, Count1, Exceptions1,
+                                        Vector2, Count2, Exceptions2) andalso AccIn;
+                          error ->
+                              false andalso AccIn
+                      end
+              end,
+    orddict:fold(FoldFun, true, Vector1).
+
+%% From the paper: ``a predecessors vector with exceptions X dominates
+%% another vector Y if the respective PVs without the exceptions
+%% dominate, and no exception included in X is dominated by Y.''
+%%
+%% This subroutine is performed for *each* actor, called from
+%% @ref{dominate/2}.
+%%
+dominates(Actor, YVector, YCount, _YExceptions, _XVector, XCount, XExceptions) ->
+    %% Does the predecessor vector dominate?
+    Dominated = XCount >= YCount,
+
+    %% Does Y dominate any of X's exceptions?
+    DominatedExceptions = lists:any(fun(E) ->
+                                            dominates({Actor, E}, YVector)
+                                    end, XExceptions),
+
+    Dominated andalso not DominatedExceptions.
 
 %% @doc Merge two vectors.
 -spec merge(vector(), vector()) -> vector().
@@ -186,5 +238,57 @@ merge_test() ->
 
     A1B1 = merge(A1, B1),
     ?assertEqual(orddict:from_list([{a, {state, 1, []}}, {b, {state, 1, []}}]), A1B1).
+
+dominates_with_exceptions_test() ->
+    A0 = new(),
+
+    %% Generate actor a.
+    {ok, VersionA1, A1} = generate(a, A0),
+    ?assertMatch({a, 1}, VersionA1),
+    ?assertMatch(true, dominates(A0, A1)),
+
+    %% Learn.
+    A4 = learn({a, 4}, A1),
+    ?assertEqual(orddict:from_list([{a, {state, 4, [2, 3]}}]), A4),
+    ?assertMatch(true, dominates(A4, A4)),
+    ?assertMatch(true, dominates(A1, A4)),
+    ?assertMatch(true, dominates(A0, A4)),
+    ?assertMatch(false, dominates(A4, A0)),
+
+    A42 = learn({a, 2}, A4),
+    ?assertEqual(orddict:from_list([{a, {state, 4, [3]}}]), A42),
+    A43 = learn({a, 3}, A4),
+    ?assertEqual(orddict:from_list([{a, {state, 4, [2]}}]), A43),
+
+    ?assertMatch(true, dominates(A4, A42)),
+    ?assertMatch(false, dominates(A42, A4)),
+
+    ?assertMatch(true, dominates(A4, A43)),
+    ?assertMatch(false, dominates(A43, A4)),
+
+    ?assertMatch(false, dominates(A43, A42)),
+    ?assertMatch(false, dominates(A42, A43)).
+
+dominates_test() ->
+    A0 = new(),
+
+    %% Generate actor a.
+    {ok, VersionA1, A1} = generate(a, A0),
+    ?assertMatch({a, 1}, VersionA1),
+    ?assertMatch(true, dominates(A0, A1)),
+
+    ?assertMatch(false, dominates(VersionA1, A0)),
+    ?assertMatch(true, dominates(VersionA1, A1)),
+
+    %% Generate actor b.
+    {ok, VersionB1, B1} = generate(b, A0),
+    ?assertMatch({b, 1}, VersionB1),
+    ?assertMatch(true, dominates(A0, B1)),
+    ?assertMatch(true, strictly_dominates(A0, B1)),
+    ?assertMatch(true, dominates(B1, B1)),
+    ?assertMatch(false, dominates(A1, B1)),
+
+    {ok, _, A1B2} = generate(b, merge(B1, A1)),
+    ?assertMatch(true, strictly_dominates(A1, A1B2)).
 
 -endif.
